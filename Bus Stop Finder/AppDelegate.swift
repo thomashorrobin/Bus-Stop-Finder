8d205dffd673817dd1b7d268b168f2724917a10d
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2016-05-01 11:44:57 +1200

    Initial Commit

---
+++ Bus Stop Finder/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Bus Stop Finder
+//
+//  Created by Thomas Horrobin on 1/05/16.
+//  Copyright © 2016 Wellington City Council Extension Apps. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Bus Stop Finder/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Bus Stop Finder
+//
+//  Created by Thomas Horrobin on 1/05/16.
+//  Copyright © 2016 Wellington City Council Extension Apps. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

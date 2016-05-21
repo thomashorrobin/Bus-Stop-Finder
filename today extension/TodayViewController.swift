1938d7d63e6b67631bdfaf0cc93baecadccf7e77
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2016-05-22 11:54:52 +1200

    added today app extension

---
+++ today extension/TodayViewController.swift
@@ -0,0 +1,25 @@
+//
+//  TodayViewController.swift
+//  today extension
+//
+//  Created by Thomas Horrobin on 22/05/16.
+//  Copyright © 2016 Wellington City Council Extension Apps. All rights reserved.
+//
+
+import Cocoa
+import NotificationCenter
+
+class TodayViewController: NSViewController, NCWidgetProviding {
+
+    override var nibName: String? {
+        return "TodayViewController"
+    }
+
+    func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)!) {
+        // Update your data and prepare for a snapshot. Call completion handler when you are done
+        // with NoData if nothing has changed or NewData if there is new data since the last
+        // time we called you
+        completionHandler(.NoData)
+    }
+
+}

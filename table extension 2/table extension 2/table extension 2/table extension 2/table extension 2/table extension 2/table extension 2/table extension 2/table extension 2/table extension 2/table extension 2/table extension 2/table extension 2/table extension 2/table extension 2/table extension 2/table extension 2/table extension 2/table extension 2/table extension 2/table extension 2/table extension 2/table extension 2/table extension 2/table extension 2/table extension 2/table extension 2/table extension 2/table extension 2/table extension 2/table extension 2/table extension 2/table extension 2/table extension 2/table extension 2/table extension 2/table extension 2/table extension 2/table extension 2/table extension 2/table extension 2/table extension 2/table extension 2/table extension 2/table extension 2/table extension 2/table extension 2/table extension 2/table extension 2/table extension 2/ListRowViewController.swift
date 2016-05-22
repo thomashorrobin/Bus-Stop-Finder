3397eeb42245ceade35964e2b9692b83a9720256
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2016-05-22 19:48:46 +1200

    added new table extension

---
+++ table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/table extension 2/ListRowViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ListRowViewController.swift
+//  table extension 2
+//
+//  Created by Thomas Horrobin on 22/05/16.
+//  Copyright © 2016 Wellington City Council Extension Apps. All rights reserved.
+//
+
+import Cocoa
+
+class ListRowViewController: NSViewController {
+
+    override var nibName: String? {
+        return "ListRowViewController"
+    }
+
+    override func loadView() {
+        super.loadView()
+        //let bs = BusStop()
+        // Insert code here to customize the view
+    }
+
+}
+
